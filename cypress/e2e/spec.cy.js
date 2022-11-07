31c4ea4e168abcd14781df837a36bc4b79558d01
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-11-07 10:16:03 +0100

    Adding end to end testing scripts

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,3 +3,30 @@ describe('The Home Page', () => {
     cy.visit('/');
   });
 });
+
+describe('Closing modal', () => {
+  it('Closing modal', () => {
+    cy.get('#registerModal')
+      .should('be.visible')
+      .find('.modal-footer')
+      .find('button[type="reset"]')
+      .click();
+  });
+});
+
+describe('Logging user in', () => {
+  it('Open login modal', () => {
+    cy.get('header').find('button[data-auth="login"]').click();
+  });
+
+  it('Logging in user', () => {
+    cy.find('#loginForm')
+      .find('#loginEmail')
+      .type('ChrTon31899@stud.noroff.no')
+      .find('#loginPassword')
+      .type('VolvoXc70!')
+      .get('.modal-footer')
+      .find('button[data-bs-dismiss="modal"]')
+      .click();
+  });
+});
