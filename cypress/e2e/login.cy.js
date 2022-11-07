41677f85ee89af86de32e5f08a8d05cf2db6c8c9
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-11-07 12:54:23 +0100

    Changes to the cypress script

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -18,31 +18,7 @@ describe('Closing register modal', () => {
   });
 });
 
-describe('Testing login with correct input values', () => {
-  const email = Cypress.env('API_EMAIL');
-  const password = Cypress.env('API_PASSWORD');
-
-  it('Open login modal', () => {
-    cy.get('header').find('button[data-auth="login"]').click();
-  });
-
-  it('Adding login credentials', () => {
-    cy.get('#loginForm').wait(500);
-    cy.get('#loginEmail')
-      .wait(500)
-      .type(`${email}`)
-      .wait(500)
-      .get('#loginPassword')
-      .type(`${password}`)
-      .wait(500)
-      .get('.modal-footer')
-      .find('button')
-      .contains('Login')
-      .click()
-      .wait(2000);
-  });
-});
-
+// Login testing with incorrect credentials
 describe('Testing login with incorrect email', () => {
   const email = 'fakeemail@fakeEmail.com';
   const password = Cypress.env('API_PASSWORD');
@@ -70,6 +46,7 @@ describe('Testing login with incorrect email', () => {
   });
 });
 
+// Login testing with incorrect credentials
 describe('Testing login with incorrect password and Email', () => {
   const email = 'fakeemail@fakeEmail.com';
   const password = 'pass';
@@ -95,4 +72,53 @@ describe('Testing login with incorrect password and Email', () => {
       .click()
       .wait(2000);
   });
+
+  // Login testing with correct credentials
+  describe('Testing login with correct input values', () => {
+    const email = Cypress.env('API_EMAIL');
+    const password = Cypress.env('API_PASSWORD');
+
+    it('Open login modal', () => {
+      cy.get('header').find('button[data-auth="login"]').click();
+    });
+
+    it('Adding login credentials', () => {
+      cy.get('#loginForm').wait(500);
+      cy.get('#loginEmail')
+        .clear()
+        .wait(500)
+        .type(`${email}`)
+        .wait(500)
+        .get('#loginPassword')
+        .clear()
+        .type(`${password}`)
+        .wait(500)
+        .get('.modal-footer')
+        .find('button')
+        .contains('Login')
+        .click()
+        .wait(2000);
+    });
+
+    it('Open new form', () => {
+      cy.get('footer')
+        .wait(500)
+        .find('a')
+        .contains('New Post')
+        .wait(200)
+        .url()
+        .should('include', 'post')
+        .click();
+    });
+  });
+
+  // Logout testing
+  // describe('Logging out', () => {
+  //   it('successfully loads', () => {
+  //     cy.visit('/')
+  //     .wait(2000)
+  //     .get('button[data-auth="logout"]')
+  //     .click();
+  //   });
+  // });
 });
--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -1,7 +1,2 @@
-// Original file content.
-// export const apiUrl = new URL("https://nf-api.onrender.com/api/v1");
-// export const apiPath = apiUrl.toString()
-
-// Content added for testing
-export const apiUrl = new URL('https://fake-api.com/');
+export const apiUrl = new URL('https://nf-api.onrender.com/api/v1');
 export const apiPath = apiUrl.toString();
