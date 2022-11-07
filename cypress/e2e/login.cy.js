909b1087fe4ffa7cc3fdfcac19fdbede55e93ca5
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-11-07 12:34:23 +0100

    Adding changes to cyprerss testing

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -18,7 +18,7 @@ describe('Closing register modal', () => {
   });
 });
 
-describe('Opening login modal', () => {
+describe('Testing login with correct input values', () => {
   const email = Cypress.env('API_EMAIL');
   const password = Cypress.env('API_PASSWORD');
 
@@ -33,6 +33,60 @@ describe('Opening login modal', () => {
       .type(`${email}`)
       .wait(500)
       .get('#loginPassword')
+      .type(`${password}`)
+      .wait(500)
+      .get('.modal-footer')
+      .find('button')
+      .contains('Login')
+      .click()
+      .wait(2000);
+  });
+});
+
+describe('Testing login with incorrect email', () => {
+  const email = 'fakeemail@fakeEmail.com';
+  const password = Cypress.env('API_PASSWORD');
+
+  it('Open login modal', () => {
+    cy.get('header').find('button[data-auth="login"]').click();
+  });
+
+  it('Adding login credentials', () => {
+    cy.get('#loginForm').wait(500);
+    cy.get('#loginEmail')
+      .clear()
+      .wait(500)
+      .type(`${email}`)
+      .wait(500)
+      .get('#loginPassword')
+      .clear()
+      .type(`${password}`)
+      .wait(500)
+      .get('.modal-footer')
+      .find('button')
+      .contains('Login')
+      .click()
+      .wait(2000);
+  });
+});
+
+describe('Testing login with incorrect password and Email', () => {
+  const email = 'fakeemail@fakeEmail.com';
+  const password = 'pass';
+
+  it('Open login modal', () => {
+    cy.get('header').find('button[data-auth="login"]').click();
+  });
+
+  it('Adding login credentials', () => {
+    cy.get('#loginForm').wait(500);
+    cy.get('#loginEmail')
+      .clear()
+      .wait(500)
+      .type(`${email}`)
+      .wait(500)
+      .get('#loginPassword')
+      .clear()
       .type(`${password}`)
       .wait(500)
       .get('.modal-footer')
