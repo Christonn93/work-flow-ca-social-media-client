cd4ebd06ef678a4d2779a30f04ce21e1c147a7bf
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-11-09 12:15:46 +0100

    Adding test failure on login unit test

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -41,11 +41,10 @@ describe('Login', () => {
   });
 });
 
-// THIS should test if the function for failure. By adding this a error comes up. Issue on this is create.
-/*describe('Login', () => {
+describe('Login', () => {
   it('Failure', async () => {
     global.fetch = jest.fn(() => fetchFailure());
     const item = await login(data);
-    expect(item).toBe(data);
+    expect(item).toBe(null || undefined);
   });
-}); */
+});
