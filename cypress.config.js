8f69b39bff7bb4bc41daf5590eebd6348367e687
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-11-09 08:21:45 +0100

    Changes to cypress congif

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
     video: false,
     setupNodeEvents(on, config) {
